fc998e324ef7a20eefa8ca12c90aca6b565b36f3
Selim <ebeeeeebebe569@gmail.com>
2026-01-03 21:30:40 +0300

    Add files via upload

---
+++ Rectangle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp3
+{
+    class Rectangle
+    {
+        private int a, b;
+
+        public Rectangle(int a, int b)
+        {
+            this.a = a;
+            this.b = b;
+        }
+
+        public int Area()
+        {
+            return a * b;
+        }
+        public int Perimeter()
+        {
+            return 2 * (a + b);
+        }
+    }
+}
